e47f72a2fcf7dca81ac8b7d0952ecc068e3173f9
Truong Loc <nguyentruongloc996@gmail.com>
2022-09-21 07:41:15 +0700

    Create Grpc Service with gRPC Template
    Create GrpcGreeter server service with the gRPC Template of Visual Studio

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // create a channel in port 5001 which is server port.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            // transmiss the channel into the constructor of Server Client.
+            var client = new Greeter.GreeterClient(channel);
+            // Call for SayHello service, and wait for response.
+            var reply = await client.SayHelloAsync(
+                    new HelloRequest { Name = "SWN Client" }
+                );
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
